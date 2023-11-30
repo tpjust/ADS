85774827b613f238f488c09f0ac383227b306e7e
tpjust <152534313+tpjust@users.noreply.github.com>
2023-12-01 01:36:41 +0530

    Update adjacency list.c

--- adjacency list.c
+++ adjacency list.c
@@ -1,76 +1,68 @@
-// Adjascency List representation in C
-
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
-  int vertex;
-  struct node* next;
-};
-struct node* createNode(int);
-
-struct Graph {
-  int numVertices;
-  struct node** adjLists;
+struct Node {
+    int data;
+    struct Node* next;
 };
 
-// Create a node
-struct node* createNode(int v) {
-  struct node* newNode = malloc(sizeof(struct node));
-  newNode->vertex = v;
-  newNode->next = NULL;
-  return newNode;
-}
+struct List {
+    struct Node* head;
+};
 
-// Create a graph
-struct Graph* createAGraph(int vertices) {
-  struct Graph* graph = malloc(sizeof(struct Graph));
-  graph->numVertices = vertices;
+#define MAX 100
 
-  graph->adjLists = malloc(vertices * sizeof(struct node*));
+struct List adjacencyList[MAX];
 
-  int i;
-  for (i = 0; i < vertices; i++)
-    graph->adjLists[i] = NULL;
-
-  return graph;
-}
-
-// Add edge
-void addEdge(struct Graph* graph, int s, int d) {
-  // Add edge from s to d
-  struct node* newNode = createNode(d);
-  newNode->next = graph->adjLists[s];
-  graph->adjLists[s] = newNode;
-
-  // Add edge from d to s
-  newNode = createNode(s);
-  newNode->next = graph->adjLists[d];
-  graph->adjLists[d] = newNode;
-}
-
-// Print the graph
-void printGraph(struct Graph* graph) {
-  int v;
-  for (v = 0; v < graph->numVertices; v++) {
-    struct node* temp = graph->adjLists[v];
-    printf("\n Vertex %d\n: ", v);
-    while (temp) {
-      printf("%d -> ", temp->vertex);
-      temp = temp->next;
+void initializeGraph(int vertices) {
+    for (int i = 0; i < vertices; i++) {
+        adjacencyList[i].head = NULL;
     }
-    printf("\n");
-  }
+}
+
+void addEdge(int start, int end) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = end;
+    newNode->next = adjacencyList[start].head;
+    adjacencyList[start].head = newNode;
+
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = start;
+    newNode->next = adjacencyList[end].head;
+    adjacencyList[end].head = newNode;
+}
+
+void printGraph(int vertices) {
+    printf("Adjacency List Representation:\n");
+    for (int i = 0; i < vertices; i++) {
+        struct Node* current = adjacencyList[i].head;
+        printf("Vertex %d: ", i);
+        while (current != NULL) {
+            printf("%d ", current->data);
+            current = current->next;
+        }
+        printf("\n");
+    }
 }
 
 int main() {
-  struct Graph* graph = createAGraph(4);
-  addEdge(graph, 0, 1);
-  addEdge(graph, 0, 2);
-  addEdge(graph, 0, 3);
-  addEdge(graph, 1, 2);
+    int vertices, edges;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &vertices);
 
-  printGraph(graph);
+    initializeGraph(vertices);
 
-  return 0;
+    printf("Enter the number of edges: ");
+    scanf("%d", &edges);
+
+    printf("Enter the edges (start end):\n");
+    for (int i = 0; i < edges; i++) {
+        int start, end;
+        scanf("%d %d", &start, &end);
+        addEdge(start, end);
+    }
+
+    printGraph(vertices);
+
+    return 0;
 }
