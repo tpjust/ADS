0aa34a43f1842e7030b085ad40f877ef172dc58e
tpjust <152534313+tpjust@users.noreply.github.com>
2023-12-01 01:35:31 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -1,85 +1,111 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct queue
 {
     int size;
     int f;
     int r;
-    int* arr;
+    int *arr;
 };
 
-
-int isEmpty(struct queue *q){
-    if(q->r==q->f){
+int isEmpty(struct queue *q)
+{
+    if (q->r == q->f)
+    {
         return 1;
     }
     return 0;
 }
 
-int isFull(struct queue *q){
-    if(q->r==q->size-1){
+int isFull(struct queue *q)
+{
+    if (q->r == q->size - 1)
+    {
         return 1;
     }
     return 0;
 }
 
-void enqueue(struct queue *q, int val){
-    if(isFull(q)){
+void enqueue(struct queue *q, int val)
+{
+    if (isFull(q))
+    {
         printf("This Queue is full\n");
     }
-    else{
+    else
+    {
         q->r++;
         q->arr[q->r] = val;
-        // printf("Enqued element: %d\n", val);
     }
 }
 
-int dequeue(struct queue *q){
+int dequeue(struct queue *q)
+{
     int a = -1;
-    if(isEmpty(q)){
+    if (isEmpty(q))
+    {
         printf("This Queue is empty\n");
     }
-    else{
+    else
+    {
         q->f++;
         a = q->arr[q->f];
     }
     return a;
 }
-int main() {
+
+int main()
+{
     struct queue q;
     q.size = 400;
     q.f = q.r = 0;
-    q.arr = (int*) malloc(q.size*sizeof(int));
+    q.arr = (int *)malloc(q.size * sizeof(int));
+
+    // User-defined input for graph
+    int size;
+    printf("Enter the size of the graph: ");
+    scanf("%d", &size);
+
+    int a[size][size];
+    printf("Enter the adjacency matrix:\n");
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int startNode;
+    printf("Enter the starting node: ");
+    scanf("%d", &startNode);
 
     // BFS Implementation
-    int node;
-    int i = 0;
-    int visited[7] = {0,0,0,0,0,0,0};
-    int a [7][7] = {
-        {0,1,1,0,0,0,0},
-        {1,0,0,1,0,0,0},
-        {1,0,0,1,1,0,0},
-        {0,1,1,0,0,0,0},
-        {0,0,1,0,0,1,1},
-        {0,0,0,0,1,0,1},
-        {0,0,0,0,1,1,0}
-    };
-    int j;
-    printf("%d ", i);
-    enqueue(&q, i);
+    int visited[size];
+    for (int i = 0; i < size; i++)
+    {
+        visited[i] = 0;
+    }
+
+    printf("%d ", startNode);
+    enqueue(&q, startNode);
+    visited[startNode] = 1;
+
     while (!isEmpty(&q))
     {
-        int node = dequeue(&q); //remove
-        visited[i] = 1; //mark visited
-        for (j = 0; j < 7; j++)
+        int node = dequeue(&q);
+        for (int j = 0; j < size; j++)
         {
-            if(a[node][j] ==1 && visited[j] == 0){
-                printf("%d ", j); //add to the bfs
+            if (a[node][j] == 1 && visited[j] == 0)
+            {
+                printf("%d ", j);
                 visited[j] = 1;
-                enqueue(&q, j); //add neighbors
+                enqueue(&q, j);
             }
         }
     }
+
+    free(q.arr); // Don't forget to free the allocated memory
     return 0;
 }
